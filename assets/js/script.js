20b8a76ce8955fbd1512e4e1d6ee16379501170e
choyeorang <znzneiei@gmail.com>
2017-02-27 14:09:41 +0900

    script.js modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,9 +83,9 @@ define( ['jquery', 'handlebars', 'contentTransition', 'uiAnimation', 'signalSync
 		        environment.isHubDevice = false;
 		    }
 
-		    $.signalClient(environment.isHubDevice, environment.screenId, environment.videoPlayList, "http://localhost:8080/signalr");
+            PageUiAnimation = new UiAnimation();
 
-		    PageUiAnimation = new UiAnimation();
+		    $.signalClient(environment.isHubDevice, environment.screenId, "http://localhost:8080/signalr");
 		    
 		}, 1000);
 		
